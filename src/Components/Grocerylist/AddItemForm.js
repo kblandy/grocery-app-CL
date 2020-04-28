8da94bda7000f2a24e2020e62287c2dabba8cc70
Kirstyn Blandy <kirstyn.blandy@gmail.com>
2020-04-28 17:59:52 -0400

    began refactoring code in AddItemForm and Searchlist, data being fetched but Searchlist not rendering after button is clicked

--- src/Components/Grocerylist/AddItemForm.js
+++ src/Components/Grocerylist/AddItemForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import SearchList from './SearchList';
 
 
@@ -9,12 +10,31 @@ class AddItemForm extends Component {
       this.state = {
         inputValue: [],
         submitted: false,
+        searchItems: [],
+        loaded: false
       };
 
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
 
     }
+    // api call to Edamame Food Database
+
+    getItem = () => {
+      let request = axios.get(`https://api.edamam.com/api/food-database/parser?ingr=${this.state.inputValue}&page=0&app_id=f0d6df51&app_key=da585cc03d43164f5e94d2df90eefbde`);
+
+      request.then(response => {
+        const data = response.data;
+        console.dir(data);
+        this.setState({
+          searchItems: [{
+            itemName: data.parsed[0].food.label,
+            itemImage: data.parsed[0].food.image
+          }]
+        })
+      })
+    }
+
     //when search button is clicked
     handleSubmit = (event) => {
       event.preventDefault();
@@ -26,7 +46,9 @@ class AddItemForm extends Component {
       console.log("this button has been clicked and is being processed");
 
       //logs the user's value object
-      console.log(this.state.inputValue);
+      console.log("input state value is: ", this.state.inputValue);
+      //fetches data using the user input
+      this.getItem();
 
     };
 
@@ -81,6 +103,8 @@ class AddItemForm extends Component {
               <SearchList 
                 searchValue={this.state.inputValue}
                 addItem={this.props.addItem}
+                searchItems={this.state.searchItems}
+                getItem={this.props.getItem}
                />
             </div>
           </div>
--- src/Components/Grocerylist/SearchList.js
+++ src/Components/Grocerylist/SearchList.js
@@ -1,41 +1,24 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-
-
-// const apiKey = 'da585cc03d43164f5e94d2df90eefbd';
-// const appId = 'f0d6df51';
-
-
 
 class SearchList extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             loaded: false,
             //items searched here
-            searchItems: [],
+            searchItems: this.props.searchItems,
             addItem: false,
             searchCleared: false
         }
+
+        
     }
 
     componentDidMount() {
-        let request = axios.get(`https://api.edamam.com/api/food-database/parser?ingr=${this.props.searchValue}&page=0&app_id=f0d6df51&app_key=da585cc03d43164f5e94d2df90eefbde`);
-
-        request.then(response => {
-            const data = response.data;
-            console.dir(data);
-            this.setState({
-                loaded: true,
-                searchItems: [{
-                    itemName: data.parsed[0].food.label,
-                    itemImage: data.parsed[0].food.image
-                }]
-        
-            })
+        this.setState({
+            loaded: true
         })
-            .catch(error => alert('There was an error processing. Reload page and try another search: ', error));
     }
             
     handleClick(name, image) {
@@ -60,7 +43,6 @@ class SearchList extends Component {
     
 
     render() {
-        console.log("current state is: ", this.state.searchCleared);
 
         var { loaded, searchItems } = this.state;
 
@@ -69,14 +51,14 @@ class SearchList extends Component {
         }
         if(loaded) {
 
-            if(this.state.searchCleared === true) {
+            // if(this.state.searchCleared === true) {
 
-                return (
-                    null
+            //     return (
+            //         null
 
-                )
+            //     )
 
-            } else {
+            // } else {
                 return (
 
                     <div className="container-sm">
@@ -108,7 +90,7 @@ class SearchList extends Component {
         };
         
     };
-};
+
 
     
 export default SearchList;
